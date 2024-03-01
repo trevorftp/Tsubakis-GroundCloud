bd2e4a5dea469035f03027ccc306aa1bf6e8d7fd
Trevor <facebookuser1068@gmail.com>
2024-03-01 09:59:56 -0500

    Update Version.js

--- Version.js
+++ Version.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name 	Tsubaki's GroundCloud
 // @namespace	https://github.com/trevorftp
-// @downloadURL https://github.com/trevorftp/Tsubakis-GroundCloud/blob/main/Tsubaki's%20GroundCloud.user.js
+// @downloadURL https://github.com/trevorftp/Tsubakis-GroundCloud/raw/main/Tsubaki's%20GroundCloud.user.js
 // @version		0.0.5
 // ==/UserScript==
